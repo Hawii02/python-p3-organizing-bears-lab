a40d8c848e1e3d6900e5a78227506fb9c12249a1
leon <onditileon21@gmail.com>
2024-03-23 23:26:31 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'Brown', 'Cool', 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 19, 'M', 'Black', 'Rash', 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'F', 'Pink', 'Calm', 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 14, 'F', 'Brown', 'Composed', 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 11, 'F', 'White', NULL, 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 15, 'M', 'Black', NULL, 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'F', 'Yellow', 'Cool', 1);
+INSERT into bears(name, age, sex, color, temperament, alive) VALUES (NULL, 13, 'F', 'White', 'Fiery', 1);
